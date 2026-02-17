e0ad6635e0bc80a2efc86c0f93c9a8411a44495f
Geminy3 <aljo.m@icloud.com>
2026-02-17 11:46:41 +0100

    update test.py

--- test.py
+++ test.py
@@ -5,21 +5,24 @@ if __name__ == "__main__":
         base_url='https://opendata.justice-administrative.fr/recherche/api/', 
     )
 
-    response = client._query(
-            params={
-                'keywords' : '"trouble anormal de voisinage"', 
-                'date_start' : None,#'2021-01-20',
-                'date_end' : None, #'2023-01-01',
-                'type' : None,#"Ordonnance",
-                'juridiction' : None,
-                'ville' : None, 
-                'OnLine' : None, #True / False
-                'nb_recherche' : 10000
-                }
-        )
+
+    query = {
+        'keywords' : '"trouble anormal de voisinage"', 
+        'date_start' : None,#'2021-01-20',
+        'date_end' : None, #'2023-01-01',
+        'type' : None,#"Ordonnance",
+        'juridiction' : None,
+        'ville' : None, 
+        'OnLine' : None, #True / False
+        'nb_recherche' : 10000
+    }
+
+    response = client._query(params = query)
     print(len(client.data['hits']))
     #print(client.data['hits'][0])
     # for dec in client.data['hits']:
     #     print(dec)
+
+    # To get a specific dec from reponses
     client.get_dec(client.data['hits'][2])
     #print(client.dec)
